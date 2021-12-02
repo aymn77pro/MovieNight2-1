8149976c049e8e931eed49c240601249dee352ee
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-12-02 13:54:23 +0300

    spelling problem

--- app/src/main/java/com/example/movienight/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/movienight/MovieDetailsFragment.kt
@@ -44,7 +44,6 @@ class MovieDetailsFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.e("TAG","idMovie:${Movie}")
         viewModel.movieInfnoData(Movie)
     }
 }
